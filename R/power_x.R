f7194ff617b8e505fb02cab5a682ea4b51c02559
yoyboy <herwineric@gmail.com>
2017-09-11 16:41:53 +0200

    geg

---
+++ R/power_x.R
@@ -0,0 +1,13 @@
+#'@title Gives u the power of x 
+#'
+#'@description This funcktion will give u the power of x
+#'@param x shall be a numeric value 
+#'@export
+#'@return NULL
+#'@examples \dontrun{
+#' # Dont run this
+#'}
+#'power_x(x=10)
+power_x<-function(x){
+  return(x^2)
+}
