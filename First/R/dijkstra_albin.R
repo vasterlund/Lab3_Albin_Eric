5ffffc9ea4aac22c36ed1bb0f19edf50e944288a
Albin Vasterlund <abbe--93@hotmail.com>
2017-09-11 15:17:04 +0200

    hej

--- First/R/dijkstra_albin.R
+++ First/R/dijkstra_albin.R
@@ -1,15 +1,19 @@
 #'@title Dijkstra algorithm
 #'
 #'@description The algorithm takes a graph and an initial node and calculates the shortest path from the initial node to every other node in the graph. 
-#'@param wiki_graph shall be a data.frame with three variables (v1, v2 and w) that contains the edges of the graph (from v1 to v2) with the weight of the edge (w).
+#'@param graph shall be a data.frame with three variables (v1, v2 and w) that contains the edges of the graph (from v1 to v2) with the weight of the edge (w).
 #'@param init_node shall be a numeric scalar that exist in the graph  
 #'@export
-#'@return NULL
-#'@examples \dontrun{
-#' # Dont run this
-#'}
-#'power_x(x=10)
-
+#'@return Return the shortest path to everyother node from the starting node as a vector. 
+#'@examples
+#'wiki_graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#'v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#'w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#'
+#'dijkstra(wiki_graph,1)
+#'dijkstra(wiki_graph,3)
+#'@references You can check how the algorithm work at it's page at wikepedia.
+#'\url{}
 dijkstra<-function(graph,init_node){
   
   if(class(graph)!="data.frame"){
@@ -60,16 +64,13 @@ dijkstra<-function(graph,init_node){
     
     datamatrial_for<-graph[(graph[,1]==rownames(datamatrial)[k]),]
     datamatrial_for<-datamatrial_for[(datamatrial_for[,2]%in%fardiga)==FALSE,]
-    
-    i=3
+     
     for(i in 1:length(fardiga)){
       datamatrial[k,colnames(datamatrial)==fardiga[i]]<-datamatrial[(k-1),colnames(datamatrial)==fardiga[i]]
     }
     
     plus<-datamatrial[(k-1),fardiga[length(fardiga)]]
     j<-0
-    i=5
-    i=18
     for(i in as.numeric(rownames(datamatrial_for))){
       j<-j+1
       senaste_varde<-datamatrial[(k-1),colnames(datamatrial)==datamatrial_for[j,2]]
@@ -87,4 +88,3 @@ dijkstra<-function(graph,init_node){
   svar<-as.numeric(datamatrial[langd,])
   return(svar)
 }
-mean
