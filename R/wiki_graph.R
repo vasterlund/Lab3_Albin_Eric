814d8580baff19b3a1267fde050f71fe8728c983
yoyboy <herwineric@gmail.com>
2017-09-13 10:50:16 +0200

    last?

---
+++ R/wiki_graph.R
@@ -0,0 +1,12 @@
+#' Length of paths betwwen differend nodes.
+#'
+#' A dataset containing 6 different nodes and the distance betwwen one another.
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{number of the first node}
+#'   \item{v2}{number of the second node that the first node goes to}
+#'   \item{w}{the distance between v1 and v2}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Graph}
+"wiki_graph"
