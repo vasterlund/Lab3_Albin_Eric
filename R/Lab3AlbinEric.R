16be56e45fdff63d62dc20e44b1329450fbcca36
Albin Vasterlund <abbe--93@hotmail.com>
2017-09-19 09:32:22 +0200

    hej

---
+++ R/Lab3AlbinEric.R
@@ -0,0 +1,19 @@
+
+
+#' Lab3AlbinEric: A package for the algorithms dijkstra and euclidean
+#'
+#' The Lab3AlbinEric package provides three categories of important functions:
+#' 
+#' @section Lab3AlbinEric functions:
+#' dijkstra
+#' euclidean
+#' 
+#' @section Lab3AlbinEric data:
+#' wiki_graph
+#'
+#' 
+#' 
+#' @name Lab3AlbinEric
+#' @docType package
+#' @author Albin Vasterlund and Eric Herwin
+NULL
